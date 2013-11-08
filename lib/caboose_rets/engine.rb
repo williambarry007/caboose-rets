f352681370581b37a9f2fb48cbabd9a7823c8853
William Barry <williambarry007@gmail.com>
2013-11-08 17:08:57 -0600

    More changes

--- lib/caboose_rets/engine.rb
+++ lib/caboose_rets/engine.rb
@@ -1,6 +1,21 @@
 require 'caboose'
 
 module CabooseRets
+  
+  def CabooseRets.get_property(mls_acct)
+    return nil if mls_acct.nil?
+    models = [
+      CabooseRets::ResidentialProperty, 
+      CabooseRets::CommercialProperty, 
+      CabooseRets::LandProperty, 
+      CabooseRets::MultiFamilyProperty
+    ]
+    models.each do |model|
+      return model.find(mls.mls_acct.to_i) if model.exists?(msg.mls_acct.to_i)            
+    end
+    return nil
+  end
+  
   class Engine < ::Rails::Engine
     isolate_namespace CabooseRets
   end
