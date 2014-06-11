ceee0e1b92f24e3178dcbb19af36dae47aeb24eb
William Barry <williambarry007@gmail.com>
2014-06-11 12:44:30 -0500

    More changes

--- app/controllers/caboose_rets/rets_controller.rb
+++ app/controllers/caboose_rets/rets_controller.rb
@@ -11,20 +11,11 @@ module CabooseRets
     # POST /admin/rets/import
     def admin_import
       return if !user_is_allowed('properties', 'edit')
+                  
+      mls_acct = params[:mls_acct].to_i            
+      ResidentialProperty.delay.import_from_mls(mls_acct)        
       
       resp = Caboose::StdClass.new
-      
-      mls_acct = params[:mls_acct].to_i      
-      
-      case params[:type]
-        when 'RES' then ResidentialProperty.delay.import_from_mls(mls_acct)
-        when 'COM' then CommercialProperty.delay.import_from_mls(mls_acct)
-        when 'LND' then LandProperty.delay.import_from_mls(mls_acct)
-        when 'MUL' then MultiFamilyProperty.delay.import_from_mls(mls_acct)
-        else
-          resp.error = "Invalid property type."
-      end
-      
       resp.success = "The property is being imported from MLS.  This may take a few minutes depending on how many images it has."
       render :json => resp
     end
