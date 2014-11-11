6016c6e63a3fd75060f8b5c6981ae0abc996b398
William Barry <williambarry007@gmail.com>
2014-11-11 10:12:27 -0600

    More changes

--- app/models/caboose_rets/rets_importer.rb
+++ app/models/caboose_rets/rets_importer.rb
@@ -436,7 +436,7 @@ class CabooseRets::RetsImporter # < ActiveRecord::Base
       if (DateTime.now - self.last_purged).to_i > 1
         self.purge
         self.save_last_purged(task_started)
-        overlap = 1.month
+        #overlap = 1.month
       end
       self.update_after(self.last_updated - overlap)
 		  self.save_last_updated(task_started)
