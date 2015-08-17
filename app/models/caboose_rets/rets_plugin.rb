095fad5919758408a6e79cdce28c5a5b61c22b1a
William Barry <williambarry007@gmail.com>
2015-08-17 16:01:48 -0500

    More changes

--- app/models/caboose_rets/rets_plugin.rb
+++ app/models/caboose_rets/rets_plugin.rb
@@ -2,21 +2,7 @@ class CabooseRets::RetsPlugin < Caboose::CaboosePlugin
 
   def self.admin_nav(nav, user = nil, page = nil, site)  
     return nav if user.nil?
-    return nav if !site.use_rets
-    
-    nav << {
-      'id' => 'saved-properties',
-      'text' => 'Saved Properties', 
-      'href' => '/saved-properties',
-      'modal' => false
-    }
-    nav << {
-      'id' => 'saved-searches',
-      'text' => 'Saved Searches', 
-      'href' => '/saved-searches',
-      'modal' => true
-    }    
-    
+    return nav if !site.use_rets    
     return nav if !user.is_allowed('properties', 'view')
     
     item = {
@@ -24,15 +10,16 @@ class CabooseRets::RetsPlugin < Caboose::CaboosePlugin
       'text' => 'RETS', 
       'children' => [],
       'modal' => true
-    }
-    
-    item['children'] << { 'id' => 'agents'      , 'href' => '/admin/agents'           , 'text' => 'Agents'                , 'modal' => false }
-    item['children'] << { 'id' => 'offices'     , 'href' => '/admin/offices'          , 'text' => 'Offices'               , 'modal' => false }
-    item['children'] << { 'id' => 'open-houses' , 'href' => '/admin/open-houses'      , 'text' => 'Open Houses'           , 'modal' => false }
-    item['children'] << { 'id' => 'residential' , 'href' => '/admin/residential'      , 'text' => 'Residential Property'  , 'modal' => false }
-    item['children'] << { 'id' => 'commercial'  , 'href' => '/admin/commercial'       , 'text' => 'Commercial Property'   , 'modal' => false }
-    item['children'] << { 'id' => 'commercial'  , 'href' => '/admin/multi-family'     , 'text' => 'Multi-Family Property' , 'modal' => false }
-    item['children'] << { 'id' => 'land'        , 'href' => '/admin/land'             , 'text' => 'Land Property'         , 'modal' => false }    
+    }    
+    item['children'] << { 'id' => 'agents'           , 'href' => '/admin/agents'           , 'text' => 'Agents'                , 'modal' => false }
+    item['children'] << { 'id' => 'offices'          , 'href' => '/admin/offices'          , 'text' => 'Offices'               , 'modal' => false }
+    item['children'] << { 'id' => 'open-houses'      , 'href' => '/admin/open-houses'      , 'text' => 'Open Houses'           , 'modal' => false }
+    item['children'] << { 'id' => 'residential'      , 'href' => '/admin/residential'      , 'text' => 'Residential Property'  , 'modal' => false }
+    item['children'] << { 'id' => 'commercial'       , 'href' => '/admin/commercial'       , 'text' => 'Commercial Property'   , 'modal' => false }
+    item['children'] << { 'id' => 'commercial'       , 'href' => '/admin/multi-family'     , 'text' => 'Multi-Family Property' , 'modal' => false }
+    item['children'] << { 'id' => 'land'             , 'href' => '/admin/land'             , 'text' => 'Land Property'         , 'modal' => false }
+    item['children'] << { 'id' => 'saved-properties' , 'href' => '/saved-properties'       , 'text' => 'Saved Properties'      , 'modal' => false }
+    item['children'] << { 'id' => 'saved-searches'   , 'href' => '/saved-searches'         , 'text' => 'Saved Searches'        , 'modal' => false }    
     
     nav << item
     
