23be88aec0ae36a3ebdd05891b7b44473cf89f50
billyswift <billy@nine.is>
2018-10-09 10:12:12 -0500

    More changes

--- app/models/caboose_rets/rets_plugin.rb
+++ app/models/caboose_rets/rets_plugin.rb
@@ -3,7 +3,7 @@ class CabooseRets::RetsPlugin < Caboose::CaboosePlugin
   def self.admin_nav(nav, user = nil, page = nil, site)  
     return nav if user.nil?
     return nav if !site.use_rets    
-    return nav if !user.is_allowed('properties', 'view')
+    return nav if !user.is_allowed('rets_properties', 'view')
     
     item = {
       'id' => 'rets',
@@ -11,10 +11,10 @@ class CabooseRets::RetsPlugin < Caboose::CaboosePlugin
       'children' => [],
       'modal' => true
     }    
-    item['children'] << { 'id' => 'agents', 'icon' => 'users',    'href' => '/admin/agents'           , 'text' => 'Agents'                , 'modal' => false }
+    item['children'] << { 'id' => 'agents', 'icon' => 'users',    'href' => '/admin/agents'           , 'text' => 'Agents'                , 'modal' => false }  if user.is_allowed('rets_agents','view')
     # item['children'] << { 'id' => 'offices'          , 'href' => '/admin/offices'          , 'text' => 'Offices'               , 'modal' => false }
     # item['children'] << { 'id' => 'open-houses'      , 'href' => '/admin/open-houses'      , 'text' => 'Open Houses'           , 'modal' => false }
-    item['children'] << { 'id' => 'properties'      , 'icon' => 'rets', 'href' => '/admin/properties'      , 'text' => 'Properties'  , 'modal' => false }
+    item['children'] << { 'id' => 'properties'      , 'icon' => 'rets', 'href' => '/admin/properties'      , 'text' => 'Properties'  , 'modal' => false }  if user.is_allowed('rets_properties','view')
     # item['children'] << { 'id' => 'commercial'       , 'href' => '/admin/commercial'       , 'text' => 'Commercial Property'   , 'modal' => false }
     # item['children'] << { 'id' => 'commercial'       , 'href' => '/admin/multi-family'     , 'text' => 'Multi-Family Property' , 'modal' => false }
     # item['children'] << { 'id' => 'land'             , 'href' => '/admin/land'             , 'text' => 'Land Property'         , 'modal' => false }
