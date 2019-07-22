1d388eb53f525857c81fbb252936a714f9762bf2
Billy Swift <billyswifty@gmail.com>
2019-07-22 09:50:41 -0500

    More changes

--- app/models/caboose_rets/rets_plugin.rb
+++ app/models/caboose_rets/rets_plugin.rb
@@ -14,12 +14,12 @@ class CabooseRets::RetsPlugin < Caboose::CaboosePlugin
     item['children'] << { 'id' => 'agents', 'icon' => 'users',    'href' => '/admin/agents'           , 'text' => 'Agents'                , 'modal' => false }  if user.is_allowed('rets_agents','view')
     # item['children'] << { 'id' => 'offices'          , 'href' => '/admin/offices'          , 'text' => 'Offices'               , 'modal' => false }
     item['children'] << { 'id' => 'open-houses', 'icon' => 'calendars', 'href' => '/admin/open-houses'      , 'text' => 'Open Houses'           , 'modal' => false } if user.is_allowed('rets_open_houses','view')
-    item['children'] << { 'id' => 'properties'      , 'icon' => 'rets', 'href' => '/admin/properties'      , 'text' => 'Properties'  , 'modal' => false }  if user.is_allowed('rets_properties','view')
+    item['children'] << { 'id' => 'properties'      , 'icon' => 'rets', 'href' => '/admin/properties'     , 'text' => 'Properties'  , 'modal' => false }  if user.is_allowed('rets_properties','view')
     # item['children'] << { 'id' => 'commercial'       , 'href' => '/admin/commercial'       , 'text' => 'Commercial Property'   , 'modal' => false }
     # item['children'] << { 'id' => 'commercial'       , 'href' => '/admin/multi-family'     , 'text' => 'Multi-Family Property' , 'modal' => false }
     # item['children'] << { 'id' => 'land'             , 'href' => '/admin/land'             , 'text' => 'Land Property'         , 'modal' => false }
     # item['children'] << { 'id' => 'saved-properties' , 'href' => '/saved-properties'       , 'text' => 'Saved Properties'      , 'modal' => false }
-    # item['children'] << { 'id' => 'saved-searches'   , 'href' => '/saved-searches'       , 'text' => 'Saved Searches'        , 'modal' => false }    
+    # item['children'] << { 'id' => 'saved-searches'   , 'href' => '/saved-searches'         , 'text' => 'Saved Searches'        , 'modal' => false }    
     
     
     nav << item
